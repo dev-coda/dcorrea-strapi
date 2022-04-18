72b88cd29c83d07848b0191ab0684be2fe5dde92
DavidCorreaV <david.correav@gmail.com>
2022-04-17 22:58:42 -0500

    fixes for initial commit config

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "564d99ccc09bd495"; }
+/******/ 		__webpack_require__.h = function() { return "ec6d735df772be09"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
